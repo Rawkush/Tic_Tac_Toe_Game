f11a44254a8e6531364ee600fba16a62f4860342
rawkush <ankushrawat950@gmail.com>
2018-06-20 16:25:22 +0530

    animating the images

--- app/src/main/java/com/example/ankush/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/ankush/tictactoegame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ankush.tictactoegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void dropIn(View view){
+
+        ImageView counter= (ImageView) view;
+
+        counter.setTranslationY(1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationY(1000f).rotation(360).setDuration(300);
+
+
+
+    }
+
+
 
 }
