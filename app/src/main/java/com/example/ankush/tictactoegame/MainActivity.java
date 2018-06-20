e02a1b9e6ecc7722f5596d7f34d94c4f0935e563
rawkush <ankushrawat950@gmail.com>
2018-06-20 16:56:13 +0530

    setting wining positions

--- app/src/main/java/com/example/ankush/tictactoegame/MainActivity.java
+++ app/src/main/java/com/example/ankush/tictactoegame/MainActivity.java
@@ -8,8 +8,9 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     int activePlay=0;  //0= yellow, 1= red
-
     int [] gameState={2,2,2,2,2,2,2,2,2}; //2 means nothing int the slot
+    int [][]winningPositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}}; // winnig poistions
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +26,35 @@ public class MainActivity extends AppCompatActivity {
 
         counter.setTranslationY(1000f);
 
-        if(activePlay==0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlay=1;
+
+
+        int tappedCounter= Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter]==2) {
+
+            gameState[tappedCounter] = activePlay;        // alloting the clicked slot the to appropriate user
+
+            if (activePlay == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlay = 1;
+            } else {
+                counter.setImageResource(R.drawable.yellow);
+                activePlay = 0;
+            }
+
+
         }
-        else {
-            counter.setImageResource(R.drawable.yellow);
-        activePlay=0;
-        }
+
         counter.animate().translationY(1000f).rotation(360).setDuration(300);
 
+        for(int []WinningPositions: winningPositions){   // taking the wiining poisiton one ata time e.g {0,1,2}
 
+            if(gameState[WinningPositions[0]]==gameState[WinningPositions[1]]&& gameState[WinningPositions[2]]==gameState[WinningPositions[0]]){
+
+
+            }
+
+        }
 
     }
 
